162e88f7741c201c6bf221fbb378503c122367dc
shipk <shipkovk@gmail.com>
2019-12-21 23:41:27 +0300

    thymeleaf completed

---
+++ src/main/java/romaniencoder/bookingdemo/ViewController.java
@@ -0,0 +1,18 @@
+package romaniencoder.bookingdemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+
+@Controller
+public class ViewController {
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("datetime", new Date());
+        model.addAttribute("username", "zxc");
+        model.addAttribute("mode","development");
+        return "index";
+    }
+}
