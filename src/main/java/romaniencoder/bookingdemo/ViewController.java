6304563a90e8aaf2b49b04d2933b5a9a96a01153
shipk <shipkovk@gmail.com>
2019-12-22 18:36:08 +0300

    App properties completed

--- src/main/java/romaniencoder/bookingdemo/ViewController.java
+++ src/main/java/romaniencoder/bookingdemo/ViewController.java
@@ -1,5 +1,8 @@
 package romaniencoder.bookingdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +11,18 @@ import java.util.Date;
 
 @Controller
 public class ViewController {
+    private String appMode;
+
+    @Autowired
+    public ViewController(Environment environment){
+        appMode = environment.getProperty("app-mode");
+    }
+
     @RequestMapping("/")
     public String index(Model model) {
         model.addAttribute("datetime", new Date());
         model.addAttribute("username", "zxc");
-        model.addAttribute("mode","development");
+        model.addAttribute("mode", appMode);
         return "index";
     }
 }
